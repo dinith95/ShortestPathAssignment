12760e1d4c31c3a166c8ac6ef573542501e44613
dinith72 <jdinith72@gmail.com>
2024-07-21 19:27:20 +0530

    added the document db repo

---
+++ AspireApp1.ApiService/DataAccess/DataAccessModels.cs
@@ -0,0 +1,29 @@
+﻿namespace AspireApp1.ApiService.DataAccess;
+
+public interface IEntity
+{ 
+    string id { get; set; }
+    // Type is the partition key
+    string Type { get; set; }
+}
+
+// graphName is the partition key 
+[DocumentDbEntity(CollectionName = "Nodes")]
+public class Node : IEntity
+{
+    public string id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+
+}
+
+// graphName is the partition key
+[DocumentDbEntity(CollectionName = "Edges")]
+public class Edge : IEntity
+{
+    public string id { get; set; }
+    public string Source { get; set; }
+    public string Destination { get; set; }
+    public int Distance { get; set; }
+    public string Type { get; set; }
+}
---
+++ AspireApp1.ApiService/DataAccess/DocumentDbRepo.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System.Linq.Expressions;
+
+namespace AspireApp1.ApiService.DataAccess;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class DocumentDbEntity : Attribute
+{
+    public string CollectionName { get; set; } = string.Empty;
+}
+
+public interface IDocumentDbRepo<T> where T : IEntity
+{
+    Task CreateItemAsync(T item);
+    Task DeleteItemAsync(T item);
+    Task<List<T>> QueryItemsAsync(Expression<Func<T, bool>> filterFunc, Expression<Func<T, T>> selectFunc);
+    Task UpdateItemAsync(T item);
+}
+
+public class DocumentDbRepo<T> : IDocumentDbRepo<T> where T : IEntity
+{
+
+    private readonly string _databaseId;
+    private CosmosClient _client;
+
+    public DocumentDbRepo()
+    {
+        string connStr = "AccountEndpoint=https://shortest-route.documents.azure.com:443/;AccountKey=uz3uutdZIr2Nw4QQd0O3il3AjPuXrPEyuiBdE3aWRtoXqkfgkHESzSemEBgtDxBKmXJAlEWv9o8dACDbBbHOtQ==;";
+        _databaseId = "shortestRouteDb";
+        _client = new CosmosClient(connStr);
+    }
+
+    public async Task<List<T>> QueryItemsAsync(Expression<Func<T, bool>> filterFunc, Expression<Func<T, T>> selectFunc)
+    {
+        var collectionName = GetCollectionName();
+       
+        var container = _client.GetContainer(_databaseId, collectionName);
+
+        var results = new List<T>();
+        var query = container.GetItemLinqQueryable<T>(true)
+                                            .Where(filterFunc)
+                                            .Select(selectFunc)
+                                            .AsQueryable();
+
+        var iterator = query.ToFeedIterator();
+
+        while (iterator.HasMoreResults)
+            results.AddRange(await iterator.ReadNextAsync());
+
+        return results;
+    }
+
+    public async Task CreateItemAsync(T item)
+    {
+        var collectionName = GetCollectionName();
+        var partitionKey = new PartitionKey(item.Type);
+        var container = _client.GetContainer(_databaseId, collectionName);
+        await container.CreateItemAsync(item, partitionKey: partitionKey);
+    }
+
+    public async Task UpdateItemAsync(T item)
+    {
+        var collectionName = GetCollectionName();
+        var partitionKey = new PartitionKey(item.Type);
+        var container = _client.GetContainer(_databaseId, collectionName);
+        await container.ReplaceItemAsync(item, item.id, partitionKey: partitionKey);
+    }
+
+    public async Task DeleteItemAsync(T item)
+    {
+        var collectionName = GetCollectionName();
+        var partitionKey = new PartitionKey(item.Type);
+        var container = _client.GetContainer(_databaseId, collectionName);
+        await container.DeleteItemAsync<T>(item.id, partitionKey: partitionKey);
+    }
+
+    private string GetCollectionName()
+    {
+        var attribute = (DocumentDbEntity)Attribute.GetCustomAttribute(typeof(T), typeof(DocumentDbEntity));
+        if (attribute == null)
+        {
+            throw new InvalidOperationException("DocumentDbEntity attribute not found");
+        }
+        return attribute.CollectionName;
+    }
+}
